317ba93779feb051c8a8b95eba6afe6e76d84b1d
Rayyan Salman <ryaansalman@gmail.com>
2026-05-28 20:21:42 +0500

    handle parameters

--- commands/timer/timer.c
+++ commands/timer/timer.c
@@ -4,3 +4,14 @@
 #include <time.h>
 #include <sys/wait.h>
 
+int main(int argc, char * argv[]){
+    if (argc<2){
+        fprintf(stderr, "Usage: %s <program_name> [parameters]\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+    char * program_name = argv[1];
+    char ** parameters = malloc(sizeof(char*)*(argc-1));
+    for(int i=0;i<argc-1;i++){
+        parameters[i]=argv[i+1];
+    }
+}
