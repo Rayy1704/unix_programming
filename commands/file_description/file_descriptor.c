9fcfcd9d926034607c0082e9b0e1bf6c12c54a45
Rayyan Salman <ryaansalman@gmail.com>
2026-06-02 16:11:00 +0000

    implement basic file descriptor retrieval functionality

--- commands/file_description/file_descriptor.c
+++ commands/file_description/file_descriptor.c
@@ -1,4 +1,16 @@
 #include <fcntl.h>
+#include <stdio.h>  
+#include <stdlib.h>
 
+int main(int argc, char * argv[]){
+    int val;
+    if(argc!=2){
+        fprintf(stderr, "Usage : %s <descriptor>\n",argv[0]);
+        return 0;
+    }
+    if ((val=fcntl(atoi(argv[1]),F_GETFL,0))<0){
+        fprintf(stderr, "fcntl error for file descriptor : %s\n",argv[1]);
+    }
+}
 
 
