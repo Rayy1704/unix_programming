f0c5507307bd3adae3f25be23f2e8ac192b1a814
Rayyan Salman <ryaansalman@gmail.com>
2026-06-04 01:28:45 +0500

    correct return value on usage error and improve output formatting

--- commands/file_description/file_descriptor.c
+++ commands/file_description/file_descriptor.c
@@ -6,7 +6,7 @@ int main(int argc, char * argv[]){
     int val;
     if(argc!=2){
         fprintf(stderr, "Usage : %s <descriptor>\n",argv[0]);
-        return 0;
+        return 1;
     }
     if ((val=fcntl(atoi(argv[1]),F_GETFL,0))<0){
         fprintf(stderr, "fcntl error for file descriptor : %s\n",argv[1]);
@@ -14,11 +14,11 @@ int main(int argc, char * argv[]){
     switch(val & O_ACCMODE){
 
         case O_RDONLY:
-        printf("Read Only\n");
+        printf("Read Only");
         break;
 
         case O_WRONLY:
-        printf("Write Only\n");
+        printf("Write Only");
         break;
 
         case O_RDWR:
@@ -28,6 +28,24 @@ int main(int argc, char * argv[]){
         default:
         fprintf(stderr, "Unknown Access Mode");
     }
+    if(val&O_APPEND){
+        printf(", append\n");
+    }
+    if (val&O_NONBLOCK){
+        printf(", nonblocking");
+    }
+    #if defined(O_SYNC)
+        if(val& O_SYNC){
+            printf(", synchronous writes");
+        }
+    #endif
+    #if !defined(_POSIC_C_SOURCE) && defined(O_FSYNC)
+        if (val&O_FSYNC){
+            printf(", synchronous writes");
+        }
+    #endif
+    putchar('\n');
+    return 0;
 }
 
 
