163d0b1635e2687ecf36eb3e46a6817c7c5909e6
Rayyan Salman <ryaansalman@gmail.com>
2026-06-03 17:01:40 +0500

    add access mode output for file descriptor retrieval

--- commands/file_description/file_descriptor.c
+++ commands/file_description/file_descriptor.c
@@ -11,6 +11,23 @@ int main(int argc, char * argv[]){
     if ((val=fcntl(atoi(argv[1]),F_GETFL,0))<0){
         fprintf(stderr, "fcntl error for file descriptor : %s\n",argv[1]);
     }
+    switch(val & O_ACCMODE){
+
+        case O_RDONLY:
+        printf("Read Only\n");
+        break;
+
+        case O_WRONLY:
+        printf("Write Only\n");
+        break;
+
+        case O_RDWR:
+        printf("Read + Write");
+        break;
+
+        default:
+        fprintf(stderr, "Unknown Access Mode");
+    }
 }
 
 
